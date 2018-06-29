ea03787129de5240e8deff0953fd7640843a6b46
wangsheng <wangsheng@cndoppler.cn>
2018-06-29 11:29:23 +0800

    修改：getMem.sh

--- getMem.sh
+++ getMem.sh
@@ -6,16 +6,18 @@ write_xml()
 {
 	echo "<disk>" >> $FILE
 	echo "<name>$1</name>" >> $FILE
-	echo "<free>$3</free>" >> $FILE
 	echo "<total>$2</total>" >> $FILE
+	echo "<used>$3</used>" >> $FILE
+	echo "<free>$4</free>" >> $FILE
 	echo "</disk>" >> $FILE
 }
 
 get_disk_info()
 {
 	disk=`mount | grep "$1" | awk '{print $3}'`
-	total=`df $disk | grep "$disk" | awk '{print $2}'`
-	free=`df $disk | grep "$disk" | awk '{print $4}'`
+	total=`df -h $disk | grep "$disk" | awk '{print $2}'`
+	used=`df -h $disk | grep "$disk" | awk '{print $3}'`
+	free=`df -h $disk | grep "$disk" | awk '{print $4}'`
 	if [ "/" == "$disk" ]
 	then
 		disk="system"
@@ -32,6 +34,6 @@ echo "<disks>" > $FILE
 for content in `echo $contents`
 do
 	get_disk_info $content
-	write_xml $disk $total $free
+	write_xml $disk $total $used $free
 done
 echo "</disks>" >> $FILE
