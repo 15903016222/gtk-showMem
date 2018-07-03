072f87c2161a4d07b3fe3e55f276a9248fe23f6e
wangsheng <wangsheng@cndoppler.cn>
2018-07-03 15:15:03 +0800

    修改：现实对话框为about对话框

--- showmem.c
+++ showmem.c
@@ -14,18 +14,6 @@ struct disk_info {
 
 struct dialog {
 	GtkWidget *mem_window;
-	GtkWidget *mem_box;
-
-	GtkWidget *system_frame;
-	GtkWidget *system_label;
-
-	GtkWidget *doppler_frame;
-	GtkWidget *doppler_label;
-
-	GtkWidget *usb_frame;
-	GtkWidget *usb_label;
-
-	GtkWidget *ok_button;
 };
 
 struct memory_dialog {
@@ -38,79 +26,18 @@ static void  init_window (struct memory_dialog *dialog);
 static void *get_mem_file (void *);
 static void  parser_mem_file (struct memory_dialog *dialog);
 
-static void free_mem (GtkWidget *widget, gpointer data)
-{
-	struct memory_dialog *mem_dialog = (struct memory_dialog *)data;
-
-	free (mem_dialog);
-	mem_dialog = NULL;
-
-	return ;
-}
-
 static void init_window (struct memory_dialog *dialog) {
 	struct memory_dialog *mem_dialog = dialog;
+	
+	mem_dialog->mem_ui.mem_window = gtk_about_dialog_new ();
 
-	mem_dialog->mem_ui.mem_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title (GTK_WINDOW (mem_dialog->mem_ui.mem_window), 
-						  "Storage Info");
-	gtk_widget_set_size_request (mem_dialog->mem_ui.mem_window, 400, 300);
-	gtk_window_set_position (GTK_WINDOW (mem_dialog->mem_ui.mem_window), 
-							 GTK_WIN_POS_CENTER);
-	gtk_window_set_resizable (GTK_WINDOW (mem_dialog->mem_ui.mem_window), FALSE);
-	gtk_container_set_border_width (GTK_CONTAINER (mem_dialog->mem_ui.mem_window), 10);
+	gtk_window_set_decorated (GTK_WINDOW (mem_dialog->mem_ui.mem_window), FALSE);
+	gtk_about_dialog_set_name (GTK_ABOUT_DIALOG (mem_dialog->mem_ui.mem_window), "Phascan Mem");
+	
+	gtk_window_set_position (GTK_WINDOW (mem_dialog->mem_ui.mem_window), GTK_WIN_POS_CENTER);
+	gtk_widget_set_size_request (mem_dialog->mem_ui.mem_window, 500, 400);
 
-	mem_dialog->mem_ui.mem_box = gtk_vbox_new (FALSE, 0);
-	gtk_container_add (GTK_CONTAINER (mem_dialog->mem_ui.mem_window), 
-					   mem_dialog->mem_ui.mem_box);
-
-	mem_dialog->mem_ui.system_frame = gtk_frame_new ("System");
-	mem_dialog->mem_ui.system_label = gtk_label_new (NULL);
-	gtk_container_add (GTK_CONTAINER (mem_dialog->mem_ui.system_frame), 
-						mem_dialog->mem_ui.system_label);
-	gtk_label_set_justify (GTK_LABEL (mem_dialog->mem_ui.system_label), 
-						   GTK_JUSTIFY_LEFT);
-	gtk_container_add (GTK_CONTAINER (mem_dialog->mem_ui.mem_box), 
-				       mem_dialog->mem_ui.system_frame);
-
-	mem_dialog->mem_ui.doppler_frame = gtk_frame_new ("Doppler");
-	mem_dialog->mem_ui.doppler_label = gtk_label_new (NULL);
-	gtk_container_add (GTK_CONTAINER (mem_dialog->mem_ui.doppler_frame), 
-						mem_dialog->mem_ui.doppler_label);
-	gtk_label_set_justify (GTK_LABEL (mem_dialog->mem_ui.doppler_label), 
-						GTK_JUSTIFY_LEFT);
-	gtk_container_add (GTK_CONTAINER (mem_dialog->mem_ui.mem_box), 
-						mem_dialog->mem_ui.doppler_frame);
-
-	mem_dialog->mem_ui.usb_frame = gtk_frame_new ("USB");
-	mem_dialog->mem_ui.usb_label = gtk_label_new (NULL);
-	gtk_container_add (GTK_CONTAINER(mem_dialog->mem_ui.usb_frame), 
-						mem_dialog->mem_ui.usb_label);
-	gtk_label_set_justify (GTK_LABEL(mem_dialog->mem_ui.usb_label), 
-						GTK_JUSTIFY_LEFT);
-	gtk_container_add (GTK_CONTAINER (mem_dialog->mem_ui.mem_box), 
-						mem_dialog->mem_ui.usb_frame);
-
-	mem_dialog->mem_ui.ok_button = gtk_button_new_with_label ("Ok");
-	gtk_container_add (GTK_CONTAINER (mem_dialog->mem_ui.mem_box), 
-						mem_dialog->mem_ui.ok_button);
-
-	g_signal_connect (mem_dialog->mem_ui.mem_window,
-					  "destroy",
-					  G_CALLBACK (gtk_main_quit),
-					  NULL);
-
-	g_signal_connect (mem_dialog->mem_ui.ok_button, 
-				      "clicked",
-				      G_CALLBACK (free_mem),
-				      mem_dialog);
-
-	g_signal_connect_swapped (mem_dialog->mem_ui.ok_button, 
-				              "clicked",
-				              G_CALLBACK (gtk_widget_destroy),
-				              mem_dialog->mem_ui.mem_window);
-
-	gtk_widget_show_all (mem_dialog->mem_ui.mem_window);
+	//	gtk_dialog_run (GTK_DIALOG (mem_dialog->mem_ui.mem_window));
 
 	return ;
 }
@@ -138,28 +65,19 @@ static void create_xml_file (void)
 	return ;
 }
 
+static gchar msg[1024] = {0};
+
 static void set_mem_label_text (struct memory_dialog *dialog)
 {
 	struct memory_dialog *mem_dialog = dialog;
-	gchar content[215] = {0};
 
-	sprintf (content, "总容量：%s  已用：%s  可用：%s", 
-			 mem_dialog->mem_info.total, 
-			 mem_dialog->mem_info.used, 
-			 mem_dialog->mem_info.free);
+	sprintf (msg, "%s 总容量：%s 已用：%s 可用：%s \n", 
+				mem_dialog->mem_info.name,
+				mem_dialog->mem_info.total, 
+				mem_dialog->mem_info.used, 
+				mem_dialog->mem_info.free);
 
-	if (!strcmp (mem_dialog->mem_info.name, "system")) {
-		gtk_label_set_text (GTK_LABEL (mem_dialog->mem_ui.system_label), 
-							content);
-	} 
-	else if (!strcmp (mem_dialog->mem_info.name, "doppler")) {
-		gtk_label_set_text (GTK_LABEL (mem_dialog->mem_ui.doppler_label), 
-							content);
-	} 
-	else if (!strcmp (mem_dialog->mem_info.name, "usb")) {
-		gtk_label_set_text (GTK_LABEL (mem_dialog->mem_ui.usb_label), 
-							content);
-	} 
+	gtk_about_dialog_set_comments (GTK_ABOUT_DIALOG (mem_dialog->mem_ui.mem_window), msg);
 
 	return ;
 }
@@ -238,6 +156,10 @@ void show_mem_info (void)
 
 	// 解析文件 并显示
 	parser_mem_file (mem_dialog);
+	
+	gtk_dialog_run (GTK_DIALOG (mem_dialog->mem_ui.mem_window));
+	gtk_widget_destroy (mem_dialog->mem_ui.mem_window);
+	free (mem_dialog);
 
 	return ;
 }
